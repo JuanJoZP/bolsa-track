f27276f4451d70cd9552c6850aab5893ad2956a8
juanescortes01 <125831607+juanescortes01@users.noreply.github.com>
2023-05-26 18:02:12 -0600

    query_one.py

--- query_one.py
+++ query_one.py
@@ -11,3 +11,11 @@ def q_2():
     count(*)
     FROM empresa e
     group by(e.id_sector)"""
+
+def q_4():
+        return """SELECT
+	(select nombre from sector s where e.id_sector=s.id),
+    count(*)
+    FROM empresa e
+    where e.ticker in(select ticker from accion where valor_actual >1000)
+    group by(e.id_sector)"""
